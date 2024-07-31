060d0027def8779e6137c84012ed22b6b6262ad1
Eduarda Calderan <eduardanicolodicalderan@gmail.com>
2024-07-31 19:49:23 -0300

    first commit

---
+++ CHAPTER 2/end_to_end_machine_learning_project/housing_file_download.py
@@ -0,0 +1,24 @@
+import constants as const
+import tarfile 
+import urllib.request
+import os
+import pandas as pd
+
+# Download data
+def fetch_housing_data(housing_url=const.HOUSING_URL, housing_path=const.HOUSING_PATH):
+	os.makedirs(housing_path, exist_ok=True)
+	tgz_path = os.path.join(housing_path, 'housing.tgz')
+	urllib.request.urlretrieve(housing_url, tgz_path)
+	housing_tgz = tarfile.open(tgz_path)
+	housing_tgz.extractall(path=housing_path)
+	housing_tgz.close()
+	
+fetch_housing_data()
+
+# Load data with pandas
+def load_housing_data(housing_path=const.HOUSING_PATH):
+	csv_path = os.path.join(housing_path, 'housing.csv')
+	return pd.read_csv(csv_path)
+
+housing = load_housing_data()
+print(housing.head())
